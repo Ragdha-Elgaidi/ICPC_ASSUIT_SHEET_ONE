7023d7b1480ae69939ca1eeb279b98dcd22c4de1
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-11-11 06:39:49 +0200

    Create C. Simple Calculator.cpp

---
+++ C. Simple Calculator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+	long long  X, Y;
+	cin >> X >> Y;
+	cout <<X<< " + "<<Y<<" = " << X + Y << endl;
+	cout <<X<< " * "<<Y<<" = " << X * Y << endl;
+	cout <<X<< " - "<<Y<<" = " << X - Y << endl;
+	return 0;
+}
