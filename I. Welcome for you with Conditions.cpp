b466aa1b1e8f49f0a31b0a5033c41eb9d1ffeb57
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-11-11 08:23:24 +0200

    Create I. Welcome for you with Conditions.cpp

---
+++ I. Welcome for you with Conditions.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{ 
+	double num1, num2;
+	cin >> num1 >> num2;
+	if (num1 >= num2)
+		cout << "Yes";
+	else
+		cout << "No";
+	return 0;
+}
