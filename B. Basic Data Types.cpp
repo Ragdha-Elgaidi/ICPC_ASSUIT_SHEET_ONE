de72576edfbbe9b820e817cc5fad4ce8d3db422a
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-11-11 06:27:18 +0200

    Create B. Basic Data Types.cpp

---
+++ B. Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int num1;
+	long long num2;
+	char a;
+	float num3;
+	double num4;
+	cin >> num1 >> num2 >> a >> num3 >> num4;
+	cout << num1 << endl;
+	cout << num2 << endl;
+	cout <<a<<endl;
+	cout << num3 << endl;
+	cout << num4 << endl;
+  return 0;
+}
