38e84b316f473e4bda2ec85721a5a984df9f42a5
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-11-11 06:52:01 +0200

    Create D. Difference.cpp

---
+++ D. Difference.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{ 
+	long long  X,A,B,C,D;
+	cin >> A >> B >> C >> D;
+	X = (A*B) - (C*D);
+	cout << "Difference = " << X << endl;
+	return 0;
+}
