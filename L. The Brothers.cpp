a51f499287ba12360341521b3c1906ffd5b26d85
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-11-22 10:21:30 +0200

    Create L. The Brothers.cpp

---
+++ L. The Brothers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string f1, f2, s1, s2;
+	cin >> f1 >> s1 >> f2 >> s2;
+	if (s1 == s2)
+		cout << "ARE Brothers" << endl;
+	else
+		cout << "NOT" << endl;
+
+}
