10c74f044661c21fb48fdf57ec5ea769caafc02e
Ragdha Elgaid <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-11-22 11:21:45 +0200

    Create P. First digit !.cpp

---
+++ P. First digit !.cpp
@@ -0,0 +1,13 @@
+    #include <iostream>
+    using namespace std;
+    int main()
+    {
+    	int X;
+    	cin >> X;
+    	int Y = X / 1000;
+    	if (Y % 2 == 0)
+    		cout << "EVEN" << endl;
+    	else
+    		cout << "ODD" << endl;
+     
+    }
